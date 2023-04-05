d80634eb5f1bbfbb4472b99b6e71181c818d32ad
KarlMarkusVahe <113974010+KarlMarkusVahe@users.noreply.github.com>
2023-04-05 14:03:45 +0300

    Kujunduse rakendamine komponendile

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,7 +4,7 @@ import ExpenseDate from "./expenseDate";
 function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <ExpenseDate date={props.expenseData.date}></ExpenseDate>
+            <div className="expense-item__date"><ExpenseDate date={props.expenseData.date}></ExpenseDate></div>
             <div className="expense-item__description">
                 <h2>{props.expenseData.title}</h2>
                 <div className="expense-item__price">{props.expenseData.price}</div>
