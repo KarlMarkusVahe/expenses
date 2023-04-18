24ef71ee0e6fbc6a11b22c80ceca01bda0d668c4
KarlMarkusVahe <113974010+KarlMarkusVahe@users.noreply.github.com>
2023-04-18 10:49:08 +0300

    Sündmuste töötlus komponentides

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,25 @@
 import './ExpenseItem.css'
 import ExpenseDate from "./expenseDate";
+import Card from "./Card";
+import {useState} from "react";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.expenseData.title)
+
+    const clickHandler = () => {
+        setTitle('updated!')
+        console.log(title)
+    }
+
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <div className="expense-item__date"><ExpenseDate date={props.expenseData.date}></ExpenseDate></div>
             <div className="expense-item__description">
                 <h2>{props.expenseData.title}</h2>
                 <div className="expense-item__price">{props.expenseData.price}</div>
             </div>
-        </div>
+            <button onClick={clickHandler}>Change Title</button>
+        </Card>
     )
 }
 
