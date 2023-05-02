19f56c48d834f94fc05327de0f19597c933f897b
KarlMarkusVahe <113974010+KarlMarkusVahe@users.noreply.github.com>
2023-05-02 09:26:25 +0300

    Filter WIP

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import ExpenseItem from "./components/Expenses/ExpenseItem";
 import NewExpense from "./components/NewExpense/NewExpense";
+import ExpensesFilter from "./components/Expenses/ExpensesFilter";
 
 function App() {
     const expenses = [
@@ -15,6 +16,11 @@ function App() {
             date: new Date(2023, 0, 10),
             title: 'New jeans',
             price: 99.99
+        },
+        {
+            date: new Date(2024, 0, 18),
+            title: 'New bag',
+            price: 198.99
         }
     ]
 
@@ -26,8 +32,10 @@ function App() {
   return (
     <div className="App">
         <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
-      <ExpenseItem expenseData={expenses[0]}></ExpenseItem>
+        <ExpensesFilter></ExpensesFilter>
+        <ExpenseItem expenseData={expenses[0]}></ExpenseItem>
         <ExpenseItem expenseData={expenses[1]}></ExpenseItem>
+        <ExpenseItem expenseData={expenses[2]}></ExpenseItem>
     </div>
   );
 }
